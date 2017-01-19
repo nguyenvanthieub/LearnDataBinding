dcf30ec5ad3598d1b6a7602b1dab68ef1d182238
Nguyen Van Thieu B <nguyen.van.thieub@framgias-Mac-mini-6.local>
2017-01-19 13:18:25 +0700

    Demo Simple

--- app/src/main/java/com/thieumao/learndatabinding/MainActivity.java
+++ app/src/main/java/com/thieumao/learndatabinding/MainActivity.java
@@ -1,13 +1,19 @@
 package com.thieumao.learndatabinding;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.thieumao.learndatabinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Leo Linh Tinh", "Thieu Mao");
+        binding.setUser(user);
+        user.setFirstName("Mot Mao");
     }
 }
---
+++ app/src/main/java/com/thieumao/learndatabinding/User.java
@@ -0,0 +1,32 @@
+package com.thieumao.learndatabinding;
+
+/**
+ * Created by nguyen.van.thieub on 1/19/17.
+ */
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
